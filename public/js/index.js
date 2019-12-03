23230e897f66215a20047ba44e2b44a91c247ee7
amasofteng <amasofteng@gmail.com>
2019-12-03 08:33:01 +0800

    5th redesign with breadcrumbs

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,65 @@
-
 $(document).ready(function () {
+
+  // select all accordion
+  $('div.head, div.content .title').on('click', function(){
+    // select the 1st accordion
+    $("#1st").click(function(){
+      // change folder to close if opened
+      $("#2nd").find('i.folder').removeClass("open",true);
+    });
+    // select the 2nd accordion
+    $("#2nd").click(function(){
+      // change folder to close if opened
+      $("#1st").find('i.folder').removeClass("open",true);
+    });
+    // toggle folder to open folder
+    $(this).find('i.folder').toggleClass("open");
+  });
+
+  // select all accordion
+  $('div.head a, div.content .title a, div.content a').on('click', function(){
+    // remove all color
+    $('.activated').removeClass('activated');
+    // add color when clicked
+    $(this).addClass('activated');
+  });
+
+  // initialize accordion
   $('.ui.accordion').accordion();
 
+  // initialize dropdown
   $('.ui.dropdown').dropdown({
+    // dropdown withouth changing main
     action: 'hide'
   });
 
-
-
-
-    // $('.accordion div.title a').on("click", function() {
-    //   $(this).hasClass('active').addClass('activated');
-    // });
-
+  // initialize sidebar
   $('#sidebarCollapse').on('click', function () {
+    // toggle on off for sidebar
     $('#sidebar').toggleClass('active');
+    // for small size toggler
+    $('#content').toggleClass('active');
+    // toggle active folder
+    var toggleA = $('i.fa-align-left');
+    var toggleB = $('i.angle.double')
+    if($('#sidebar').hasClass('active')){
+      toggleA.removeClass('fas fa-align-left');
+      toggleA.addClass('angle double right icon');
+    } else {
+      toggleB.removeClass('angle double right icon');
+      toggleB.addClass('fas fa-align-left');
+    }
   });
 
-$('div.title, div.content').on('click', function () {
-
-    $(this.title).addClass('activated');
-
-});
+  // sidebar folder to sub nav breadcrumb
+  $('.ui.accordion a').on('click', function() {
+    var $this = $(this);
+    var $bc = $('<div></div>');
+    $this.parents('li').each(function(n, li) {
+      var $a = $(li).children('div').children('a').clone();
+      $bc.prepend($a);
+    });
+    $('#location1').html($bc.prepend(''));
+  });
 
 });
--- public/js/user.js
+++ public/js/user.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
+
+  // click signup signin button
   $(".cta").click(function() {
+
+    // toggle active to cta parents
     $(this).parents().toggleClass("active");
+
+    // initialize change text
     var name = $(".sign-up").text();
+
+    // change text and icons when active
     if(name === "SIGN-UP"){
       name = $(".sign-up").text("SIGN-IN");
       $(".cc").text("SIGN-UP ...");
@@ -13,6 +21,8 @@ $(document).ready(function () {
       $("#ccc").removeClass("signup");
       $("#ccc").addClass("sign-in");
     }
+
+    // end the click with active content
     return $(".content-ctr").toggleClass("active");
   });
 });
